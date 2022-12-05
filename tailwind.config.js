3c0257aa0ddbe2839456602303f95a416ba91802
Morshed Alam <ma.morshedalampro@gmail.com>
2022-12-05 16:02:18 +0600

    project init

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["*.{html,js}"],
+  darkMode: 'class',
+  theme: {
+    fontFamily: {
+      
+    },
+    screens: {
+      'xs': '380px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',      
+    },
+  },
+  plugins: [],
+}
